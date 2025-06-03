30a47005415bda3b85446840fd9c6deb626a4c16
Flloyd Nathaniel Gagni <fnbg720@gmail.com>
2025-06-03 15:36:56 +0800

    Add scripts to make the notes card and notes list draggable
    Signed-off-by: Flloyd Nathaniel Gagni <fnbg720@gmail.com>

---
+++ scripts/title_bar.gd
@@ -0,0 +1,21 @@
+extends HBoxContainer
+
+var following : bool = false
+var dragging_start_position : Vector2 = Vector2.ZERO
+
+var window_node : Window
+
+func _ready() -> void:
+	window_node = get_parent().get_parent().get_parent() as Window
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+				following = !following
+				dragging_start_position = get_local_mouse_position()
+
+func _process(_delta: float) -> void:
+	if following:
+		var mouse_position = get_global_mouse_position()
+		var window_position = Vector2(window_node.position)
+		window_node.position = window_position + (mouse_position - dragging_start_position)
