6ec2f20b8b5ba7ed94397d65d8efae3674e98347
Flloyd Nathaniel Gagni <fnbg720@gmail.com>
2025-06-04 10:55:27 +0800

    Complete the display of notes preview
    Signed-off-by: Flloyd Nathaniel Gagni <fnbg720@gmail.com>

--- scripts/notes_container.gd
+++ scripts/notes_container.gd
@@ -2,6 +2,7 @@ extends VBoxContainer
 
 @onready var new_note_window = preload("res://scenes/notes_card.tscn")
 @onready var notes_container: VBoxContainer = $"."
+@onready var note_preview = preload("res://scenes/note_preview.tscn")
 
 func _ready() -> void:
 	NotesManager.load_notes()
@@ -9,6 +10,7 @@ func _ready() -> void:
 
 func load_all_notes_into_ui():
 	for note in NotesManager.notes:
-		var note_card = new_note_window.instantiate()
-		note_card.initialize(note)
-		get_tree().root.add_child(note_card)
+		var preview_instance = note_preview.instantiate()
+		var preview_label: RichTextLabel = preview_instance.get_node("PreviewMarginContainer/PreviewText")
+		preview_label.text = NotesManager.notes[note].note_body
+		notes_container.add_child.call_deferred(preview_instance)
